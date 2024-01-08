8a17559df65e7fabe80925c542bb86b5eafccbf1
GRIS <pogorelov.vlad2000@gmail.com>
2024-01-08 05:27:59 +0200

    Added backgrounds, ambient music, changed visuals of some UI, some polishing. Looks like finished.

--- Novel/Assets/Scripts/Nani/UI/ActiveQuestUI.cs
+++ Novel/Assets/Scripts/Nani/UI/ActiveQuestUI.cs
@@ -10,16 +10,24 @@ namespace Nani.UI
         [SerializeField] private TextMeshProUGUI _text;
         
         private ITextManager _textManager;
+        private string _currentKey = "none";
+        private string _currentCategory = ManagedTextRecord.DefaultCategoryName;
+        private LocalizationManager _localizationManager;
 
         protected override void Awake()
         {
             base.Awake();
             _textManager = Engine.GetService<ITextManager>();
+            _localizationManager = Engine.GetService<LocalizationManager>();
+            _localizationManager.OnLocaleChanged += s => SetText(_currentKey, _currentCategory);
+            SetText(_currentKey, _currentCategory);
         }
 
         public void SetText(string key, string category = ManagedTextRecord.DefaultCategoryName)
         {
-            // Debug.Log($"___Quest text: {key}");
+            Debug.Log($"___Quest text: {key} : {category}");
+            _currentKey = key;
+            _currentCategory = category;
             _text.gameObject.SetActive(key != "none");
             _text.text = _textManager.GetRecordValue(key, category);
         }
